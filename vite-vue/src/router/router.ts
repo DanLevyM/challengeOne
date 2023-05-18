b5b97d8b9577c41125c7b5792822a2d921ce5784
Léa Ifergan <leaifergan15@gmail.com>
2023-05-18 18:33:26 +0200

    prod front routes

--- vite-vue/src/router/router.ts
+++ vite-vue/src/router/router.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/home/Home.vue";
 import filmDb from "../../films.json";
 import ErrorView from "../views/stripe/Error.vue";
 import SuccessView from "../views/stripe/Success.vue";
@@ -81,17 +80,17 @@ const routes = [
   {
     path: "/success",
     name: "successview",
-    component: SuccessView,
+    component: () => import("../views/stripe/Success.vue"),
   },
   {
     path: "/error",
     name: "errorview",
-    component: ErrorView,
+    component: () => import("../views/stripe/Error.vue"),
   },
   {
     path: "/payment/:id",
     name: "Payment",
-    component: Stripe,
+    component: () => import("../views/stripe/Stripe.vue"),
   },
 ];
 
