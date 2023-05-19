16970203fd855b910426b8b99e280a63779ae165
rina-myriam <rinamyriamh@gmail.com>
2023-05-19 14:24:22 +0200

    clean syntaxe

--- vite-vue/src/views/movie/MovieItem.vue
+++ vite-vue/src/views/movie/MovieItem.vue
@@ -197,7 +197,7 @@
                                 </div>
                                 <div v-else>
                                     Aucune critique n'a été rédigée pour le moment
-                                    <p>{{review.descritpion}}{{review[0].description}}gygyu</p>
+                                    <p>{{review.descritpion}}{{review[0].description}}</p>
                                 </div>
                                 <!-- LIST COMMENTS -->
                                 <div
